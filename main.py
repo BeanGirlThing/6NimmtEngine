eeaafcd516542f2b29d86ca8c838408b76f846c9
BeanGirlThing <jessica@ferret.life>
2025-02-19 04:20:13 +0000

    Add logging

--- main.py
+++ main.py
@@ -4,13 +4,29 @@ from configparser import ConfigParser
 import players
 from prettytable import PrettyTable
 import json
+import logging
+from django.utils.text import slugify
+import os
+import sys
+import datetime
 
-PLAYER_LIST = [players.RandomPlay(), players.LowestHighest(), players.HighestLowest(), players.LowestHighestSwap()]
+
+def get_logfile_absolute_path(path: str = None):
+    if path is None:
+        path = os.getcwd() + "/logs"
+    if not os.path.exists(path):
+        os.makedirs(path)
+    time_component = slugify(str(datetime.datetime.now().strftime("%m-%d-%Y--%H-%M-%S")))
+    return f"{path}/6NimmtEngine-{time_component}.log"
+
 
 class Main:
     columns = None
     deck = None
     config = None
+    logger = None
+
+    player_list = None
 
     SHOW_GAME = False
     WAIT_FOR_MODERATOR = False
@@ -22,19 +38,56 @@ class Main:
     }
 
     def __init__(self):
+        # Logging Junk
+        self.logger = logging.getLogger("6NimmtEngine")
+        self.logger.setLevel(logging.DEBUG)
+        logging_file_handler = logging.FileHandler(get_logfile_absolute_path())
+        logging_file_handler.setLevel(logging.DEBUG)
+        logging_stream_handler = logging.StreamHandler(sys.stdout)
+        logging_stream_handler.setLevel(logging.INFO)
+
+        logging_formatter = logging.Formatter(
+            fmt='[%(asctime)s][%(levelname)s][%(name)s] - %(message)s',
+            datefmt='%d-%b-%y %H:%M:%S'
+        )
+
+        logging_file_handler.setFormatter(logging_formatter)
+        logging_stream_handler.setFormatter(logging_formatter)
+
+        self.logger.addHandler(logging_file_handler)
+        self.logger.addHandler(logging_stream_handler)
+
+        self.logger.info("Starting 6Nimmt Engine")
+
+        self.player_list = [players.RandomPlay(), players.LowestHighest(), players.HighestLowest(),
+                            players.LowestHighestSwap()]
+
+        # Read the config file
         self.config = ConfigParser()
         self.config.read("config.ini")
+        self.logger.info("Read config file successfully")
+
+        # Get SHOW_GAME variable from config
         if int(self.config["game"]["show"]) == 1:
+            self.logger.info("Game is set to be shown in console - WARNING this may cause lag")
             self.SHOW_GAME = True
+
+        # Get WAIT_FOR_MODERATOR variable from config
         if int(self.config["game"]["wait_for_moderator"]) == 1:
+            self.logger.info("Game is set to wait for moderator input between rounds")
             self.WAIT_FOR_MODERATOR = True
+
+        # Get GAMES_TO_PLAY variable from config
         self.GAMES_TO_PLAY = int(self.config["engine"]["games_to_play"])
+
         for i in range(self.GAMES_TO_PLAY):
-            print(f"Game Number {i}")
+            self.logger.info(f"Game Number {i} - Setting up")
             self.setup_game()
+            self.logger.info("Running the game")
             winner = self.game()
+            self.logger.info(f"Winner determined - {winner.name} - Building data output")
             list_of_players_in_game = []
-            for player in PLAYER_LIST:
+            for player in self.player_list:
                 list_of_players_in_game.append({
                     "name": player.name,
                     "score": player.score
@@ -45,30 +98,40 @@ class Main:
                 "winner_score": winner.score,
                 "players": list_of_players_in_game
             })
-            for player in PLAYER_LIST:
-                if not player.name in self.data["player_names"]:
+
+            self.logger.info("Resetting players to default")
+            for player in self.player_list:
+                # Sneaky addition of player names into the data file as the for loop can be used.
+                if player.name not in self.data["player_names"]:
                     self.data["player_names"].append(player.name)
                 player.reset()
 
+        self.logger.info("Writing data to datafile.json")
         with open("datafile.json", "w") as f:
             f.write(json.dumps(self.data))
 
+        self.logger.info("Done!")
+
     def setup_game(self):
+        self.logger.debug("Generating deck & shuffling")
         self.deck = Deck()
         self.deck.shuffle()
         self.columns = []
 
-        for i in range(0,4):
+        self.logger.debug("Draw the initial cards for the 4 columns")
+        for i in range(0, 4):
             self.columns.append([self.deck.deal()])
 
+        self.logger.debug("Generating player hands")
         hands = []
-        for i in range(0, len(PLAYER_LIST)):
+        for i in range(0, len(self.player_list)):
             hands.append(Hand())
         for i in range(0, 10):
             for j in range(0, len(hands)):
                 hands[j].add_card(self.deck.deal())
 
-        for i, player in enumerate(PLAYER_LIST):
+        self.logger.debug("Passing hands to players")
+        for i, player in enumerate(self.player_list):
             player.set_hand(hands[i])
 
         if self.SHOW_GAME:
@@ -78,35 +141,38 @@ class Main:
 
     def game(self) -> players.Player:
         played_cards = []
-        for i in range(0,10):
+        for i in range(0, 10):
+            self.logger.debug(f"Round {i}")
             previous_round_played_cards = []
             if played_cards:
                 previous_round_played_cards = played_cards
             played_cards = []
-            for player_index, player in enumerate(PLAYER_LIST):
+            self.logger.debug("Running the turn method within the bots")
+            for player_index, player in enumerate(self.player_list):
                 played_cards.append((player_index, player.turn(self.columns, previous_round_played_cards)))
 
-            played_cards.sort(key=lambda card: card[1].value, reverse=False)
+            self.logger.debug("Sorting and processing played cards")
+            played_cards.sort(key=lambda individual_card: individual_card[1].value, reverse=False)
             for player_index, card_played in played_cards:
                 column_to_place = None
                 column_difference = 0
 
                 # Determine whether card played is lower than all columns, if not, which column does it follow
-                for i, column in enumerate(self.columns):
-                    if card_played.value > column[len(column)-1].value:
+                for j, column in enumerate(self.columns):
+                    if card_played.value > column[len(column) - 1].value:
                         if column_to_place is None:
-                            column_to_place = i
-                            column_difference = card_played.value - column[len(column)-1].value
+                            column_to_place = j
+                            column_difference = card_played.value - column[len(column) - 1].value
                         else:
-                            if column_difference > card_played.value - column[len(column)-1].value:
-                                column_to_place = i
-                                column_difference = card_played.value - column[len(column)-1].value
+                            if column_difference > card_played.value - column[len(column) - 1].value:
+                                column_to_place = j
+                                column_difference = card_played.value - column[len(column) - 1].value
 
                 # Handle card played being lower than all columns
                 if column_to_place is None:
-                    selected_column = PLAYER_LIST[player_index].take_selected_row(self.columns, played_cards)
+                    selected_column = self.player_list[player_index].take_selected_row(self.columns, played_cards)
                     for card in self.columns[selected_column]:
-                        PLAYER_LIST[player_index].add_to_score(card.score)
+                        self.player_list[player_index].add_to_score(card.score)
 
                     self.columns[selected_column] = [card_played]
 
@@ -114,7 +180,7 @@ class Main:
                     # Handle card placed into column being 6th
                     if len(self.columns[column_to_place]) == 5:
                         for card in self.columns[column_to_place]:
-                            PLAYER_LIST[player_index].add_to_score(card.score)
+                            self.player_list[player_index].add_to_score(card.score)
                         self.columns[column_to_place] = [card_played]
                     else:
                         # If all other handles are not run, the card can be placed into the column without consequence
@@ -130,15 +196,14 @@ class Main:
                 input("(Enter to Continue)")
 
         # Determine the winner
+        self.logger.info("Game Over - Determining a winner")
         lowest_score = None
-        for player in PLAYER_LIST:
+        for player in self.player_list:
             if lowest_score is None:
                 lowest_score = player
             else:
                 if lowest_score.score > player.score:
                     lowest_score = player
-
-        print(f"{lowest_score.name} Wins!")
         return lowest_score
 
     def display_table(self):
@@ -147,7 +212,7 @@ class Main:
         for i in range(0, 6):
             row = []
             for column in self.columns:
-                if len(column)-1 < i:
+                if len(column) - 1 < i:
                     row.append(" ")
                 else:
                     row.append(f"V{column[i].value} S{column[i].score}")
@@ -159,7 +224,7 @@ class Main:
         player_names = []
         list_of_cards = []
         for card in played_cards:
-            player_names.append(PLAYER_LIST[card[0]].name)
+            player_names.append(self.player_list[card[0]].name)
             list_of_cards.append(f"V{card[1].value} S{card[1].score}")
         table.field_names = player_names
         table.add_row(list_of_cards)
@@ -169,14 +234,12 @@ class Main:
         table = PrettyTable()
         player_names = []
         player_scores = []
-        for player in PLAYER_LIST:
+        for player in self.player_list:
             player_names.append(player.name)
             player_scores.append(player.score)
         table.field_names = player_names
         table.add_row(player_scores)
         print(table)
-
-
 
 
 if __name__ == "__main__":
--- players/Player.py
+++ players/Player.py
@@ -1,14 +1,17 @@
 from hand import Hand
 from card import Card
-
+import logging
 
 class Player:
     hand: Hand = None
     name: str = None
     score: int = 0
+    logger = None
 
     def __init__(self, name: str):
         self.name = name
+        self.logger = logging.getLogger(f"6NimmtEngine.Bot - {name}")
+        self.logger.info("Initialising bot")
 
     def set_hand(self, hand: Hand):
         self.hand = hand
